d33766ef25448dbcddf9f43e492ee787431d86d2
Izabela Guerreiro <izaguerreiro@gmail.com>
2015-12-23 14:47:06 -0200

    Correção do settings

--- eventex/settings.py
+++ eventex/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': config(DATABASE_URL, default=default_dburl, cast=dburl),
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
