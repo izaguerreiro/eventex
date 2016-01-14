128763bbfbe10e8185948cf05aade637314f3873
Izabela Guerreiro <izaguerreiro@gmail.com>
2016-01-13 23:54:11 -0200

    Habilita inscrição

--- eventex/settings.py
+++ eventex/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
             ],
         },
     },
-] 
+]
 
 WSGI_APPLICATION = 'eventex.wsgi.application'
 
@@ -100,7 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/' 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Email configuration
--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -58,12 +58,12 @@ class SubscribePostTest(TestCase):
 
 	def test_subscription_email_from(self):
 		email = mail.outbox[0]
-		expect = 'contato@eventex.com.br'
+		expect = 'izaguerreiro@gmail.com'
 		self.assertEqual(expect, email.from_email)
 
 	def test_subscription_email_to(self):
 		email = mail.outbox[0]
-		expect = ['contato@eventex.com.br', 'izaguerreiro@gmail.com']
+		expect = ['izaguerreiro@gmail.com', 'izaguerreiro@gmail.com']
 		self.assertEqual(expect, email.to)
 
 	def test_subscription_email_body(self):
