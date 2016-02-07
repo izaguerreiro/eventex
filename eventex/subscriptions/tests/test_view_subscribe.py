fcff65664b042d6ca27ea81049a97bd950043d97
Izabela Guerreiro <izaguerreiro@gmail.com>
2016-02-07 19:05:51 -0200

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -44,8 +44,8 @@ class SubscribePostValid(TestCase):
 		self.resp = self.client.post('/inscricao/', data)
 
 	def test_post(self):
-		""" Valid POST should redirect /inscricao/ """
-		self.assertEqual(302, self.resp.status_code)
+		""" Valid POST should redirect /inscricao/1/ """
+		self.assertRedirects(self.resp, '/inscricao/1/')
 
 	def test_send_subscribe_email(self):
 		self.assertEqual(1, len(mail.outbox))
@@ -75,9 +75,3 @@ class SubscribePostInvalid(TestCase):
 
 	def test_dont_save_subscription(self):
 		self.assertFalse(Subscription.objects.exists())
-
-class SubscribeSuccessMessage(TestCase):
-	def test_message(self):
-		data = dict(name='Izabela Guerreiro', cpf=12345678901, email='izaguerreiro@gmail.com', phone='12-991266633')
-		response = self.client.post('/inscricao/', data, follow=True)
-		self.assertContains(response, 'Inscrição realizada com sucesso!')
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -2,7 +2,7 @@ from django.core import mail
 from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
@@ -14,26 +14,36 @@ def subscribe(request):
 	else:
 		return new(request)
 
+
 def create(request):
 	form = SubscriptionForm(request.POST)
 	
 	if not form.is_valid():
 		return render(request, 'subscriptions/subscription_form.html', {'form': form})
 
+	subscription = Subscription.objects.create(**form.cleaned_data)
+
+	#Send subscription email
 	_send_mail('Confirmação de inscrição',
 				settings.DEFAULT_FROM_EMAIL, 
-				form.cleaned_data['email'],
+				subscription.email,
 				'subscriptions/subscription_email.txt',
-				form.cleaned_data)
+				{'subscription': subscription})
 
-	Subscription.objects.create(**form.cleaned_data)
-	messages.success(request, 'Inscrição realizada com sucesso!')
-	return HttpResponseRedirect('/inscricao/')
+	return HttpResponseRedirect('/inscricao/{}/'.format(subscription.pk))
 
 	
-
 def new(request):
 	return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+
+
+def detail(request, pk):
+	try:
+		subscription = Subscription.objects.get(pk=pk)
+	except Subscription.DoesNotExist:
+		raise Http404
+	return render(request, 'subscriptions/subscription_detail.html', {'subscription': subscription})
+
 
 def _send_mail(subject, from_, to, template_name, context):
 	body = render_to_string(template_name, context)
